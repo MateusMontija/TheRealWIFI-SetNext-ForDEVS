a7021d475324a53948bd0a886196279a57a9cc89
Mateus Montija <129836411+MateusMontija@users.noreply.github.com>
2023-08-30 01:03:01 -0300

    Add files via upload

--- TheRealWIFI.py
+++ TheRealWIFI.py
@@ -1,7 +1,34 @@
 import os
 from time import sleep
+import requests
 
+sleep(1)
 
+#Animação de barra#
+print ("\n [___________] 0%")
+sleep(0.10);os.system('cls')
+print ("\n [██_________] 10%")
+sleep(0.10);os.system('cls')
+print ("\n [███________] 20%")
+sleep(0.10);os.system('cls')
+print ("\n [████_______] 30%")
+sleep(0.10);os.system('cls')
+print ("\n [█████______] 40%")
+sleep(0.10);os.system('cls')
+print ("\n [██████_____] 50%")
+sleep(0.10);os.system('cls')
+print ("\n [███████____] 60%")
+sleep(0.10);os.system('cls')
+print ("\n [████████___] 70%")
+sleep(0.10);os.system('cls')
+print ("\n [█████████__] 80%")
+sleep(0.10);os.system('cls')
+print ("\n [██████████_] 90%")
+sleep(0.20);os.system('cls')
+print ("\n [███████████] 100%")
+#Animação de barra#
+
+os.system('cls')
 
 print("""
  ████████╗██╗░░██╗███████╗  ██████╗░███████╗░█████╗░██╗░░░░░  ░██╗░░░░░░░██╗██╗███████╗██╗
@@ -10,12 +37,12 @@ print("""
  ░░░██║░░░██╔══██║██╔══╝░░  ██╔══██╗██╔══╝░░██╔══██║██║░░░░░  ░░████╔═████║░██║██╔══╝░░██║
  ░░░██║░░░██║░░██║███████╗  ██║░░██║███████╗██║░░██║███████╗  ░░╚██╔╝░╚██╔╝░██║██║░░░░░██║
  =========================================================================================
- SetNext(ForDEVS)  <------------------------------------------------------------->  v1.0.0
+ SetNext(ForDEVS)  <------------------------------------------------------------->  v1.0.1
  =========================================================================================
  
- ----------------""")
+ -------------------""")
 
-choose = input(" [1]=[COMEÇAR]\n [2]=[SAIR]\n ----------------\n ―>")
+choose = input(" [1]=[COMEÇAR]\n [2]=[SAIR]\n -------------------\n ――>")
 
 
 
@@ -41,15 +68,21 @@ def TheRealWIFI():
     PING = MyTest.results.ping
     #<--PING-->#
 
+
+    #<--PublicIP-->#
+    publicIP = requests.get('https://api.ipify.org/').text
+    #<--PublicIP-->#
+
     os.system('cls')
 
     print(f"""
  ●TheRealWIFI
- ---------------------------------
+ ==================================
+ ►PublicIP: {publicIP}        
  ►Download: {downloadSpeed / 10**6:.2f}
  ►Upload: {uploadSpeed / 10**6:.2f}
  ►PING: {PING:.2f}
- ---------------------------------""")
+ ==================================""")
     
     input(" [OK]")
     input(" ")
@@ -69,5 +102,5 @@ if choose == "2":
 
 
 os.system('cls')
-print("\n ATENÇÃO!\n Abra o programa novamente, você não digitou 1 e nem 2.")
-sleep(5)
+print("\n ATENÇÃO!\n ----------------------------\n Abra o programa novamente\n você não digitou 1 e nem 2.")
+sleep(4)
---
+++ readme.md
@@ -0,0 +1,20 @@
+# TheRealWIFI
+
+
+## Descrição
+A startup SetNext disponibilizou esse código-fonte para todos os desenvolvedores. Se trata de um programa feito com PYTHON onde você consegue ter vários dados sobre sua internet. É possível transformar a base desse código em produtos para outros tipos de desenvolvedores, gamers, empresas...
+<hr>
+
+## Versão (v1.1.0)
+Nessa nova versão foi:
++ Adicionado Experiência otimizada.
++ Mais informações de sua REDE
++ Barra de carregamento
++ Melhoria no código
++ Corrigido +8 bugs
+
+## Créditos
+Ficamos extremamente felizes em anunciar os principais colaboradores desse projeto.
++ SetNext (ForDevs)
++ MateusMontija
++ TheASK
